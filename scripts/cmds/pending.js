a67ea52b086f235df707f0e29179c24788c914d7
asta ichiyukimøri <161681849+Asta-ichiyukimori@users.noreply.github.com>
2024-03-01 03:10:19 +0100

    Create pending.js

---
+++ scripts/cmds/pending.js
@@ -0,0 +1,49 @@
+module.exports = {
+  config: {
+    name: "pending",
+    version: "1.0",
+    author: "لوفي",
+    countDown: 5,
+    role: 2,
+    shortDescription: {
+      vi: "",
+      en: ""
+    },
+    longDescription: {
+      vi: "",
+      en: ""
+    },
+    category: "admin"
+  },
+ 
+langs: {
+    en: {
+        invaildNumber: "%1 is not an invalid number",
+        cancelSuccess: "Refused %1 thread!",
+        approveSuccess: "Approved successfully %1 threads!",
+ 
+        cantGetPendingList: "Can't get the pending list!",
+        returnListPending: "»「PENDING」«❮ The whole number of threads to approve is: %1 thread ❯\n\n%2",
+        returnListClean: "「PENDING」There is no thread in the pending list"
+    }
+  },
+ 
+onReply: async function({ api, event, Reply, getLang, commandName, prefix }) {
+    if (String(event.senderID) !== String(Reply.author)) return;
+    const { body, threadID, messageID } = event;
+    var count = 0;
+ 
+    if (isNaN(body) && body.indexOf("c") == 0 || body.indexOf("cancel") == 0) {
+        const index = (body.slice(1, body.length)).split(/\s+/);
+        for (const singleIndex of index) {
+            console.log(singleIndex);
+            if (isNaN(singleIndex) || singleIndex <= 0 || singleIndex > Reply.pending.length) return api.sendMessage(getLang("invaildNumber", singleIndex), threadID, messageID);
+            api.removeUserFromGroup(api.getCurrentUserID(), Reply.pending[singleIndex - 1].threadID);
+            count+=1;
+        }
+        return api.sendMessage(getLang("cancelSuccess", count), threadID, messageID);
+    }
+    else {
+        const index = body.split(/\s+/);
+        for (const singleIndex of index) {
+            if (isNaN(singleIndex) || singleIndex <= 0 || singleIndex > Reply.pending.length) return api.sendMessage(getLang("invaildNumber", singleIndex), threadID, messageID);
